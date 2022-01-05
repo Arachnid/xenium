bbdecd33a7fd3c3369571b56ff0f3332de88ddcf
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2022-01-05 19:38:12 +0800

    single use claimant replay protection validator added

---
+++ packages/validator/contracts/SingleClaimantExecutor.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./BaseExecutor.sol";
+import "@openzeppelin/contracts/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/contracts/utils/Base64.sol";
+
+/**
+ * @dev An abstract implementation of an Executor that only allows claims with unique clamaints. 
+ *      To use, subclass and override `executeClaim` and `metadata`, being sure to call `super` inside `executeClaim` before doing anything else.
+ *      This executor expects the first 32 bytes of `claimData` to be the nonce; you may optionally use extra data for your own purposes.
+ */
+abstract contract SingleClaimantExecutor is BaseExecutor {
+
+    // claimant -> beneficiary
+    mapping (address => address) public claimants;
+    
+    error UsedClaimant();
+
+    constructor(address _validator) BaseExecutor(_validator) { }
+
+    /**
+     * @dev Executes a claim that has been verified by the `ValidatorRegistry`. Implementers must check that this function
+     *      was called by a registry they recognise, and that any conditions in claimData such as replay protection are met
+     *      before acting on the request.
+     * @param issuer The account that issued the claim.
+     * @param claimant The account that is entitled to make the claim.
+     * @param beneficiary The account that the claim should benefit.
+     * @param claimData Claim data provided by the issuer.
+     * @param executorData Contextual information stored on the ValidatorRegistry for this issuer.
+     */
+    function executeClaim(address issuer, address claimant, address beneficiary, bytes calldata claimData, bytes calldata executorData) public virtual override {
+        super.executeClaim(issuer, claimant, beneficiary, claimData, executorData);
+
+        // if claimant was already used
+        if(claimants[claimant] != address(0)) {
+            revert UsedClaimant();
+        }
+        claimants[claimant] = beneficiary;
+    }
+
+    /**
+     * @dev Returns metadata explaining a claim. Subclasses should call this first and return it if it is nonempty.
+     * @return A URL that resolves to JSON metadata as described in the spec.
+     *         Callers must support at least 'data' and 'https' schemes.
+     */
+    function metadata(address /*issuer*/, bytes calldata /*claimData*/, bytes calldata /*executorData*/) public override virtual view returns(string memory) {
+        return "";
+    }    
+}
---
+++ packages/validator/contracts/test/TestSingleClaimantExecutor.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "../SingleClaimantExecutor.sol";
+/**
+ * @dev Test implementation of a SingleClaimantExecutor that just logs the fact it was called.
+ */
+contract TestSingleClaimantExecutor is SingleClaimantExecutor {
+    event Claimed(address issuer, address beneficiary, bytes claimData, bytes executorData);
+
+    constructor(address _validator) SingleClaimantExecutor(_validator) { }
+
+    /**
+     * @dev Executes a claim that has been verified by the `ValidatorRegistry`. Implementers must check that this function
+     *      was called by a registry they recognise, and that any conditions in claimData such as replay protection are met
+     *      before acting on the request.
+     * @param issuer The account that issued the claim.
+     * @param beneficiary The account that the claim should benefit.
+     * @param claimData Claim data provided by the issuer.
+     * @param executorData Contextual information stored on the ValidatorRegistry for this issuer.
+     */
+    function executeClaim(address issuer, address claimant, address beneficiary, bytes calldata claimData, bytes calldata executorData) public override {
+        super.executeClaim(issuer, claimant, beneficiary, claimData, executorData);
+        emit Claimed(issuer, beneficiary, claimData, executorData);
+    }
+
+    /**
+     * @dev Returns metadata explaining a claim.
+     * @param issuer The address of the issuer.
+     * @param claimData Claim data provided by the issuer.
+     * @param executorData Contextual information stored on the ValidatorRegistry for this issuer.
+     * @return A URL that resolves to JSON metadata as described in the spec.
+     *         Callers must support at least 'data' and 'https' schemes.
+     */
+    function metadata(address issuer, bytes calldata claimData, bytes calldata executorData) public override virtual view returns(string memory) {
+        string memory ret = super.metadata(issuer, claimData, executorData);
+        if(bytes(ret).length > 0) {
+            return ret;
+        }
+        return string(abi.encodePacked(
+            "data:application/json;base64,",
+            Base64.encode("{\"valid\":true,\"data\":{\"title\":\"Emit an event\"}}")
+        ));
+    }
+}
