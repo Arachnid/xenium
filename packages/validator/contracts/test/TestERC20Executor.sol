94b611de58ccb71a473b3b48bd6ccbb31cb0a557
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2022-01-10 10:41:17 +0800

    ERC20 Executor merged with latest changes

--- packages/validator/contracts/test/TestERC20Executor.sol
+++ packages/validator/contracts/test/TestERC20Executor.sol
@@ -15,12 +15,13 @@ contract TestERC20Executor is ERC20Executor {
      *      was called by a registry they recognise, and that any conditions in claimData such as replay protection are met
      *      before acting on the request.
      * @param issuer The account that issued the claim.
+     * @param claimant The account that is entitled to make the claim.
      * @param beneficiary The account that the claim should benefit.
      * @param claimData Claim data provided by the issuer.
      * @param executorData Contextual information stored on the ValidatorRegistry for this issuer.
      */
-    function executeClaim(address issuer, address beneficiary, bytes calldata claimData, bytes calldata executorData) public override {
-        super.executeClaim(issuer, beneficiary, claimData, executorData);
+    function executeClaim(address issuer, address claimant, address beneficiary, bytes calldata claimData, bytes calldata executorData) public override {
+        super.executeClaim(issuer, claimant, beneficiary, claimData, executorData);
     }
 
     
@@ -28,13 +29,14 @@ contract TestERC20Executor is ERC20Executor {
     /**
      * @dev Returns metadata explaining a claim.
      * @param issuer The address of the issuer.
+     * @param claimant The account that is entitled to make the claim.
      * @param claimData Claim data provided by the issuer.
      * @param executorData Contextual information stored on the ValidatorRegistry for this issuer.
      * @return A URL that resolves to JSON metadata as described in the spec.
      *         Callers must support at least 'data' and 'https' schemes.
      */
-    function metadata(address issuer, bytes calldata claimData, bytes calldata executorData) public override virtual view returns(string memory) {
-        string memory ret = super.metadata(issuer, claimData, executorData);
+    function metadata(address issuer, address claimant, bytes calldata claimData, bytes calldata executorData) public override virtual view returns(string memory) {
+        string memory ret = super.metadata(issuer, claimant, claimData, executorData);
         if(bytes(ret).length > 0) {
             return ret;
         }
