971bdbf83d6599e70f8484e3c7c19ec3086b56d5
Nick Johnson <arachnid@notdot.net>
2022-01-27 15:21:39 +1300

    Fix deployments

--- packages/validator/deploy/00_deploy_factories.ts
+++ packages/validator/deploy/00_deploy_factories.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
-const factories = ['ERC20TransferUniqueNonceValidator'];
+const factories = ['ERC20TransferUniqueNonceValidatorFactory'];
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     const {deployer} = await hre.getNamedAccounts();
